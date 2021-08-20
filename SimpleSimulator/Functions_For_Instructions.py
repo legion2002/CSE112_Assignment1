186643f498dbd1bd8cdb1bb9018d030039001cc2
srishti_jain <srishtijain0201@gmail.com>
2021-08-20 20:37:12 +0530

    invert done

--- SimpleSimulator/Functions_For_Instructions.py
+++ SimpleSimulator/Functions_For_Instructions.py
@@ -7,10 +7,7 @@ def add(instruction, PC):
     reg1 = instruction[7: 10]
     reg2 = instruction[10: 13]
     reg3 = instruction[13: 16]
- 
-    # rf.setRegInt(reg2, 15)
-    # rf.setRegInt(reg3,15) #Use this kind of functions to set registers to some value and check your function
-
+    
     a = hp.convertReg2int(rf.getReg(reg2))
     b = hp.convertReg2int(rf.getReg(reg3))
     
@@ -20,7 +17,6 @@ def add(instruction, PC):
     if len(ansBin) > 16:
         rf.setOverflow(1)
         rf.setRegString(reg1, ansBin[len(ansBin) - 16:])
-        
     else:
         rf.setRegString(reg1, hp.convertBinary16(ans))
     
@@ -31,9 +27,6 @@ def subtraction(instruction, PC):
 	reg1 = instruction[7:10]
 	reg2 = instruction[10:13]
 	reg3 = instruction[13:16]
-
-	#rf.setRegInt(reg2, 2)
-	#rf.setRegInt(reg3,3)
 
 	operand1 = hp.convertReg2int(rf.getReg(reg2))
 	operand2 = hp.convertReg2int(rf.getReg(reg3))
@@ -53,9 +46,7 @@ def move_immediate(instruction, PC):
 	imm = instruction[8:]
 	
 	imm_value_in_int = hp.convertString2int(imm)
-	
 	bin_rep = hp.convertBinary16(imm_value_in_int)
-	
 	rf.setRegString(reg1, bin_rep)
 
 	return PC + 1
@@ -68,6 +59,7 @@ def move_register(instruction, PC):
 	reg_value = rf.getReg(reg2)
 	rf.setRegString(reg1, reg_value)
 	rf.setRegList("111",[0]*16)
+
 	return PC + 1
 
 
@@ -75,7 +67,6 @@ def load(instruction, PC, cycle):
 	reg = instruction[5:8]
 	memory = instruction[8:]
 
-	# rf.setRegInt(reg, 12)
 	plt.scatter(cycle, hp.convertString2int(memory), c = "blue")
 	mem_value = mem.getMemoryBin(memory)
 	value = hp.convertString2int(mem_value)
@@ -101,19 +92,14 @@ def multiply(instruction, PC):
     reg2 = instruction[10: 13]
     reg3 = instruction[13: 16]
  
-    # rf.setRegInt(reg2, 15)
-    # rf.setRegInt(reg3,15) #Use this kind of functions to set registers to some value and check your function
-
     a = hp.convertReg2int(rf.getReg(reg2))
     b = hp.convertReg2int(rf.getReg(reg3))
-    
     ans  = a * b
     ansBin = hp.convertBinary(ans)
    
     if len(ansBin) > 16:
         rf.setOverflow(1)
         rf.setRegString(reg1, ansBin[len(ansBin) - 16:])
-        
     else:
         rf.setRegString(reg1, hp.convertBinary16(ans))
     
@@ -124,14 +110,12 @@ def divide(instruction, PC):
 	reg = instruction[10:13]
 	reg2 = instruction[13:]
 	
-	# rf.setRegInt(reg, 15)
-	# rf.setRegInt(reg2,15) #Use this kind of functions to set registers to some value and check your function
 	a = hp.convertReg2int(rf.getReg(reg))
 	b = hp.convertReg2int(rf.getReg(reg2))
-	ans  = a / b
-	rem = a%b
 	if(b == 0):
 		print("divisor cant be 0")
+	ans  = int(a / b)
+	rem = int(a%b)
 
 	reg0 = "000"
 	reg1 = "001"
@@ -144,7 +128,7 @@ def divide(instruction, PC):
 def right_shift(instruction, PC):
 	reg1 = instruction[5:8]
 	imm = instruction[8:]
-	#rf.setRegInt(reg1, 10)
+
 	value_to_be_shifted = hp.convertReg2int(rf.getReg(reg1))
 	imm_value_in_int = hp.convertString2int(imm)
 	right_shifted = value_to_be_shifted >> imm_value_in_int
@@ -158,7 +142,7 @@ def right_shift(instruction, PC):
 def left_shift(instruction, PC):
 	reg1 = instruction[5:8]
 	imm = instruction[8:]
-	#rf.setRegInt(reg1, 5)
+
 	value_to_be_shifted = hp.convertReg2int(rf.getReg(reg1))
 	imm_value_in_int = hp.convertString2int(imm)
 	left_shifted = value_to_be_shifted << imm_value_in_int
@@ -173,9 +157,6 @@ def Exclusive_or(instruction, PC):
 	reg1 = instruction[7:10]
 	reg2 = instruction[10:13]
 	reg3 = instruction[13:16]
-
-	#rf.setRegInt(reg2, 5)
-	#rf.setRegInt(reg3,9)
 
 	value1 = hp.convertReg2int(rf.getReg(reg2))
 	value2 = hp.convertReg2int(rf.getReg(reg3))
@@ -193,14 +174,10 @@ def bitwise_or(instruction, PC):
 	reg2 = instruction[10:13]
 	reg3 = instruction[13:16]
 
-	#rf.setRegInt(reg2, 5)
-	#rf.setRegInt(reg3,9)
-
 	operand1 = rf.getReg(reg2)
 	operand2 = rf.getReg(reg3)
 
 	result = []
-
 	for i in range(len(operand1)):
 		result.append(int(int(operand1[i]) or int(operand2[i])))
 
@@ -214,14 +191,10 @@ def bitwise_and(instruction, PC):
 	reg2 = instruction[10:13]
 	reg3 = instruction[13:16]
 
-	#rf.setRegInt(reg2, 5)
-	#rf.setRegInt(reg3,9)
-
 	operand1 = rf.getReg(reg2)
 	operand2 = rf.getReg(reg3)
 
 	result = []
-
 	for i in range(len(operand1)):
 		result.append(int(int(operand1[i]) and int(operand2[i])))
 
@@ -234,14 +207,9 @@ def invert(instruction, PC):
 	reg1 = instruction[10:13]
 	reg2 = instruction[13:]
 
-	#rf.setRegInt(reg2, 5)
-	#rf.setRegInt(reg3,9)
-
 	value1 = hp.convertReg2int(rf.getReg(reg2))
-
-	result = ~value1
+	result = ~value1 & (pow(2,16)-1)
 	bin_rep = hp.convertBinary16(result)
-
 	rf.setRegString(reg1, bin_rep)
 
 	return PC + 1
@@ -250,9 +218,6 @@ def invert(instruction, PC):
 def compare(instruction, PC):
 	reg1 = instruction[10:13]
 	reg2 = instruction[13:]
-
-	#rf.setRegInt(reg2, 5)
-	#rf.setRegInt(reg3,9)
 
 	value1 = hp.convertReg2int(rf.getReg(reg1))
 	value2 = hp.convertReg2int(rf.getReg(reg2))
@@ -278,6 +243,7 @@ def small_jump(instructions,PC):
 	memory = instructions[8:]
 	mem_add = hp.convertString2int(memory)
 	smaller_than_flag = rf.getReg("111")[13]
+
 	if smaller_than_flag == 1:
 		PC = mem_add
 	else:
@@ -292,34 +258,39 @@ def greater_jump(instructions,PC):
 	memory = instructions[8:]
 	mem_add = hp.convertString2int(memory)
 	greater_than_flag = rf.getReg("111")[14]
+
 	if greater_than_flag == 1:
 		PC = mem_add
 	else:
 		PC = PC +1
+
 	rf.setRegList("111",[0]*16)
 
 	return PC
 
 
 def equal_jump(instruction, PC):
-	#rf.setRegInt("111",1)
 	mem_add = instruction[8:]
 	mem_add_in_int = hp.convertString2int(mem_add)
 	value_of_equal_flag = rf.getReg("111")[15]
+
 	if(value_of_equal_flag == 1):
 		PC = mem_add_in_int
 	else:
 		PC = PC + 1
+
 	rf.setRegList("111",[0]*16)
 
 	return PC
 
 
 def halt(instruction, PC):
+
 	print(hp.convertBinary8(PC), end = " ")
 	for register in rf.Register_Table.values():
 		print(hp.convertIntList2String(register), end =  " ")
 	print()
+
 	mem.dumpMemory()
 	plt.savefig("Bonus_Question_Plot")
 	exit()
