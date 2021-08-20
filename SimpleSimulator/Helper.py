8bac15b5b7e36c51e859ee29b292cdfeb1d4a4e3
srishti_jain <srishtijain0201@gmail.com>
2021-08-20 20:41:47 +0530

    all test casses passed

--- SimpleSimulator/Helper.py
+++ SimpleSimulator/Helper.py
@@ -1,5 +1,4 @@
 def convertBinary8(value):
-    
     binRep = bin(value).replace("0b", "")
     while(len(binRep) < 8):
         binRep = "0" + binRep
@@ -7,8 +6,8 @@ def convertBinary8(value):
     assert len(binRep) == 8, "something is wrong with the memory Value"
     return binRep
 
+
 def convertBinary16(value):
-    
     binRep = bin(value).replace("0b", "")
     while(len(binRep) < 16):
         binRep = "0" + binRep
@@ -16,24 +15,27 @@ def convertBinary16(value):
     assert len(binRep) == 16, "something is wrong with the memory Value"
     return binRep
 
+
 def convertBinary(value):
     return bin(value).replace("0b", "")
+
 
 def convertIntList2String(listInteger):
     #returns the value of a list of integers to a string (Use to read register)
     return "".join(map(str,listInteger ))
 
+
 def convertString2int(stringValue):
     #returns the value of the binary string to integer
     return int(stringValue,2)
+
 
 def convertReg2int(regList):
     # returns the value of the register as an integer
     return int("".join(map(str,regList )),2)
 
+
 def convertString2IntList(stringVal):
-
     return list(map(int, list(stringVal)))
+ 
     
-
-
